bc94a5fd1af29e5012e0c26019d04dd962050c0d
KOPO20\USER <woo4091009@naver.com>
2023-10-02 20:49:27 +0900

    TEST

--- src/main/java/kr/ac/kopo/ctc/kopo20/resort/domain/Member.java
+++ src/main/java/kr/ac/kopo/ctc/kopo20/resort/domain/Member.java
@@ -38,4 +38,5 @@ public class Member {
 	public static Member createUser(String userId, String pw, PasswordEncoder passwordEncoder) {
 		return new Member(null, userId, passwordEncoder.encode(pw), "USER");
 	}
+	//TEST
 }
