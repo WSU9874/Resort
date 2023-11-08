c3fd4b25e2d89721305cdacbbc4dec06a04332b0
KOPO20\USER <woo4091009@naver.com>
2023-11-09 01:59:20 +0900

    aws 설정 작업

--- src/main/java/kr/ac/kopo/ctc/kopo20/resort/security/WebConfig.java
+++ src/main/java/kr/ac/kopo/ctc/kopo20/resort/security/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 	 */
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:8082") // 허용할 도메인
+		registry.addMapping("/**").allowedOrigins("http://localhost:8082","http://www.polysort.site", "http://polysort.site") // 허용할 도메인
 				.allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
 				.allowCredentials(true) // 인증 정보 허용
 				.maxAge(3600); // preflight 요청 결과를 캐시할 시간(초)
